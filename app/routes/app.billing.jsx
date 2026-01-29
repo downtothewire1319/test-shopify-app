31afd51c161213f999ea32047d8524bd883c3b2b
OddSignature10857 <workspace2000.official@gmail.com>
2026-01-29 18:34:51 +0600

    upt

--- app/routes/app.billing.jsx
+++ app/routes/app.billing.jsx
@@ -2,45 +2,51 @@ import { authenticate } from "../shopify.server";
 import { redirect } from "react-router";
 
 export const loader = async ({ request }) => {
-  const { admin } = await authenticate.admin(request);
+  try {
+    const { admin } = await authenticate.admin(request);
 
-  const response = await admin.graphql(
-    `#graphql
-    mutation AppPurchaseOneTimeCreate(
-      $name: String!
-      $price: MoneyInput!
-      $returnUrl: URL!
-    ) {
-      appPurchaseOneTimeCreate(
-        name: $name
-        price: $price
-        returnUrl: $returnUrl
+    const response = await admin.graphql(
+      `#graphql
+      mutation AppPurchaseOneTimeCreate(
+        $name: String!
+        $price: MoneyInput!
+        $returnUrl: URL!
       ) {
-        confirmationUrl
-        userErrors {
-          message
+        appPurchaseOneTimeCreate(
+          name: $name
+          price: $price
+          returnUrl: $returnUrl
+        ) {
+          confirmationUrl
+          userErrors {
+            message
+          }
         }
-      }
-    }`,
-    {
-      variables: {
-        name: "Sales Analytics – Life time Access-Non refundable",
-        returnUrl: `${process.env.SHOPIFY_APP_URL}/app`,
-        price: {
-          amount: 700,
-          currencyCode: "USD",
+      }`,
+      {
+        variables: {
+          name: "Sales Analytics – One-Time Access",
+          returnUrl: "https://test-shopify-app-pi.vercel.app/app",
+          price: {
+            amount: 700,
+            currencyCode: "USD",
+          },
         },
-      },
+      }
+    );
+
+    const data = await response.json();
+
+    const confirmationUrl =
+      data?.data?.appPurchaseOneTimeCreate?.confirmationUrl;
+
+    if (!confirmationUrl) {
+      throw new Error("No confirmation URL");
     }
-  );
 
-  const data = await response.json();
-  const confirmationUrl =
-    data?.data?.appPurchaseOneTimeCreate?.confirmationUrl;
-
-  if (!confirmationUrl) {
-    throw new Error("Billing failed");
+    return redirect(confirmationUrl);
+  } catch (err) {
+    console.error("BILLING ERROR", err);
+    return redirect("/app?billing_error=true");
   }
-
-  return redirect(confirmationUrl);
 };
